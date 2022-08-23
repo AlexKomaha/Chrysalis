0e04f60e76fed860f677d0f19e17cd7e14d8ee15
Alison Jeffrey <alisonnwebb@hotmail.com>
2022-08-23 14:06:06 +0900

    Save changes

--- app/controllers/user_goals_controller.rb
+++ app/controllers/user_goals_controller.rb
@@ -1,12 +1,12 @@
 class UserGoalsController < ApplicationController
-
   def index
     @user_goal = UserGoal.where(user_id: current_user)
     @user_goals = policy_scope(UserGoal)
   end
 
   def new
-    raise
+    @user_goal = UserGoal.new
+    # authorize @user_goal
   end
 
   def create
