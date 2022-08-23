2f2142f7ca5fe424f50516075532a6f44c921cbc
AlexKomaha <107383667+AlexKomaha@users.noreply.github.com>
2022-08-23 11:11:50 +0900

    controllers

---
+++ app/controllers/user_goals_controller.rb
@@ -0,0 +1,2 @@
+class UserGoalsController < ApplicationController
+end
---
+++ test/controllers/controllers/user_goals_controller.rb_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Controllers::UserGoalsController.rbControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
