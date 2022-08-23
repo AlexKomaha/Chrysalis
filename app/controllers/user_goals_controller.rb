793d6d1da3fd26c588c3d3bb068c9e5a3690899b
Alison Jeffrey <alisonnwebb@hotmail.com>
2022-08-23 12:41:18 +0900

    Just added actions, not complete

--- app/controllers/user_goals_controller.rb
+++ app/controllers/user_goals_controller.rb
@@ -4,4 +4,12 @@ class UserGoalsController < ApplicationController
     @user_goal = UserGoal.where(user_id: current_user)
     @user_goals = policy_scope(UserGoal)
   end
+
+  def new
+    raise
+  end
+
+  def create
+    raise
+  end
 end
