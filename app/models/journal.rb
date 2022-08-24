3e0b77a2e92b92c358a84a766e34aa2e9fa85fc7
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-08-24 16:59:33 +0900

    Installed action text

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -2,4 +2,5 @@ class Journal < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { minimum: 3 }
   validates :content, presence: true, length: { minimum: 6 }
+  has_rich_text :content
 end
