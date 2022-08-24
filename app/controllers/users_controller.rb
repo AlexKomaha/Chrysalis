6e629542190b1301b44c49d9a9d58df2f65929e5
Alison Jeffrey <alisonnwebb@hotmail.com>
2022-08-24 17:37:18 +0900

    Save changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def dashboard
-    @user_goals = policy_scope(UserGoal)
+    @user_goal = UserGoal.where(user_id: current_user)
+    @user_goals = policy_scope(UserGoal).group_by(&:description)
     authorize @user_goals
   end
 end
