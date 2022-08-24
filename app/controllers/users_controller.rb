d251711ca21a708819736f447b353a68fc97f6ac
Alison Jeffrey <alisonnwebb@hotmail.com>
2022-08-24 18:08:03 +0900

    Save changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  skip_after_action :verify_authorized, only: :dashboard
+
   def dashboard
-    @user_goal = UserGoal.where(user_id: current_user)
     @user_goals = policy_scope(UserGoal).group_by(&:description)
-    authorize @user_goals
   end
 end
