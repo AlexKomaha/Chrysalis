a200396bf7f3eb53a0a5b3520b9b78c138eb48fc
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-08-24 18:16:31 +0900

    Added the action text, but needed to be fixed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,5 @@
 
 import { application } from "./application"
 
-import CheckboxController from "./checkbox_controller"
-application.register("checkbox", CheckboxController)
-
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,6 +1,6 @@
 class Journal < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { minimum: 3 }
-  validates :content, presence: true, length: { minimum: 6 }
-  has_rich_text :content
+  # validates :content, presence: true, length: { minimum: 6 }
+  has_rich_text :rich_content
 end
