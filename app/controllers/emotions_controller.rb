b539acfbaf085c16d04e3789175a83f1fd97b838
AlexKomaha <107383667+AlexKomaha@users.noreply.github.com>
2022-08-29 16:35:13 +0900

    emotions1

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,8 +1,19 @@
 class EmotionsController < ApplicationController
   def index
     @emotions = policy_scope(Emotion)
+    @emotion = Emotion.new
       # Scope your query to the dates being shown:
     start_date = params.fetch(:start_date, Date.today).to_date
     @emotions = @emotions.where(created_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+  end
+
+  def create
+    @emotion = Emotion.new(emotion_params)
+    @emotion.user = current_user
+    @emotion.save
+  end
+
+  def emotion_params
+    params.require(:emotion).permit(:emotion_level)
   end
 end
