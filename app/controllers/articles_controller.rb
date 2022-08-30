a5a2e992ad449c64e0a56992dee2db9028c7ac87
Alison Jeffrey <alisonnwebb@hotmail.com>
2022-08-30 12:59:17 +0900

    Added article cards

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @article = Article.all
-    @article = policy_scope(Article)
+    @articles = Article.all
+    @articles = policy_scope(Article)
   end
 end
--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -1,10 +1,7 @@
 class ArticlePolicy < ApplicationPolicy
   class Scope < Scope
-    # def resolve
-    #   scope.all
-    # end
     def resolve
-      scope.where(user: user)
+      scope.all
     end
   end
 end
