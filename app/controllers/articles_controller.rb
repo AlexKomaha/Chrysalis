21a0cc8c746faec304880e803d0a06b4f00de1ba
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-08-30 15:44:26 +0900

    Added the map

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,13 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
     @articles = policy_scope(Article)
+    # location
+    @locations = Location.all
+    @markers = @locations.geocoded.map do |location|
+      {
+        lat: location.latitude,
+        lng: location.longitude
+      }
+    end
   end
 end
