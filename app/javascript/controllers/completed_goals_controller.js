0bf1c47bacc9295e22ca977bc03970ab02fac217
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-08-31 13:02:01 +0900

    Added the disappear function

--- app/javascript/controllers/completed_goals_controller.js
+++ app/javascript/controllers/completed_goals_controller.js
@@ -4,39 +4,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["congrats", "checkbox", "disappear"]
   connect() {
-    console.log("Congrats")
   }
 
   sayCongrats() {
-    // let arrayB = this.checkboxTargets.map(checkbox => {
-    //   checkbox.checked
-    // })
-    // console.log(this.checkboxTargets.map(c => c.checked))
-    // const allEqual = arr => arr.every( v => v.checked === true )  // true
     const allChecked = this.checkboxTargets.every( checkbox => checkbox.checked)
     console.log(allChecked)
     // console.log(arrayB)
     if (allChecked) {
       this.congratsTarget.classList.remove('d-none')
+      this.disappearTarget.classList.add('d-none')
      }
-    // this.checkboxTargets.forEach((box) => {
-    //   // find a way to know if all have been checked
-    //   console.log(arrayB)
-
-    // })
   }
 }
-
-
-
-    // this.congratsTarget.classList.remove('d-none')
-
-    // this.disappearTarget.classList.add('d-none')
-    // this.checkboxTargets.forEach((box) => {
-    //   // find a way to know if all have been checked
-    //   console.log(box)
-    //   this.congratsTarget.classList.remove('d-none')
-
-    // })
-    // if all checked,
-    // this.congratsTarget.classList.remove('d-none')
